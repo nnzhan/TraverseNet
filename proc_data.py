c176334ad4fd23624f58c00f01ea1fe332dfb556
abcdefg-dev-dd <78794925+abcdefg-dev-dd@users.noreply.github.com>
2021-02-27 18:54:37 +0800

    Update proc_data.py

--- proc_data.py
+++ proc_data.py
@@ -2,9 +2,9 @@ from dataset.pems_d import *
 
 batch_size = 64
 num_nodes = 170
-path='/home/ubuntu/Projects/ST-benchmark/data/PEMS08/PEMS08.npz'
-adjpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS08/PEMS08.csv'
-idpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS08/PEMS08.txt'
+path='./data/PEMS08/PEMS08.npz'
+adjpath='./data/PEMS08/PEMS08.csv'
+idpath='./data/PEMS08/PEMS08.txt'
 op = pemsD(num_nodes, path, adjpath, None)
 op.prcoess('./data/PEMS-D8.pkl')
 dataloader = load_data(64, "./data/PEMS-D8.pkl")
@@ -16,9 +16,9 @@ pickle.dump(g, file)
 #
 batch_size = 64
 num_nodes = 358
-path='/home/ubuntu/Projects/ST-benchmark/data/PEMS03/PEMS03.npz'
-adjpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS03/PEMS03.csv'
-idpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS03/PEMS03.txt'
+path='./data/PEMS03/PEMS03.npz'
+adjpath='./PEMS03/PEMS03.csv'
+idpath='./data/PEMS03/PEMS03.txt'
 op = pemsD(num_nodes, path, adjpath, idpath)
 op.prcoess('./data/PEMS-D3.pkl')
 _, srclist, tgtlist, distlist = op.load_graph1()
@@ -28,9 +28,9 @@ pickle.dump(g, file)
 #
 batch_size = 64
 num_nodes = 307
-path='/home/ubuntu/Projects/ST-benchmark/data/PEMS04/PEMS04.npz'
-adjpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS04/PEMS04.csv'
-idpath='/home/ubuntu/Projects/ST-benchmark/data/PEMS04/PEMS04.txt'
+path='./data/PEMS04/PEMS04.npz'
+adjpath='./data/PEMS04/PEMS04.csv'
+idpath='./data/PEMS04/PEMS04.txt'
 op = pemsD(num_nodes, path, adjpath, None)
 op.prcoess('./data/PEMS-D4.pkl')
 _, srclist, tgtlist, distlist = op.load_graph1()
